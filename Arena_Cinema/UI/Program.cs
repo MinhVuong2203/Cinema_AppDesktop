dc81bc97abeb2cb5a47ee1b5c379443fd842a20f
MinhVuong2203 <vuonghihihihi@gmail.com>
2025-11-19 21:42:21 +0700

    Quên new Login

--- Arena_Cinema/UI/Program.cs
+++ Arena_Cinema/UI/Program.cs
@@ -16,7 +16,7 @@ namespace UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TestPayment());
+            Application.Run(new Login());
         }
     }
 }
