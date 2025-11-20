1a9d2faf87aa199ce64bd3c3f74b76444cd379a7
sHunOrinn <161719997+sHunOrinn@users.noreply.github.com>
2025-11-20 14:44:25 +0700

    thêm icon

--- Arena_Cinema/UI/Properties/Resources.Designer.cs
+++ Arena_Cinema/UI/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace UI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap add {
+            get {
+                object obj = ResourceManager.GetObject("add", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap armchair {
             get {
                 object obj = ResourceManager.GetObject("armchair", resourceCulture);
@@ -356,6 +366,16 @@ namespace UI.Properties {
         internal static System.Drawing.Bitmap minus {
             get {
                 object obj = ResourceManager.GetObject("minus", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap minus1 {
+            get {
+                object obj = ResourceManager.GetObject("minus1", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
