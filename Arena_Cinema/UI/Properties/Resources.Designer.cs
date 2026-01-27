2de8f9c2f1df487546391d1ec773b5bff0ad187d
sHunOrinn <161719997+sHunOrinn@users.noreply.github.com>
2026-01-27 18:20:43 +0700

    thêm icon

--- Arena_Cinema/UI/Properties/Resources.Designer.cs
+++ Arena_Cinema/UI/Properties/Resources.Designer.cs
@@ -583,6 +583,26 @@ namespace UI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap popcorn2 {
+            get {
+                object obj = ResourceManager.GetObject("popcorn2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap popcorn3 {
+            get {
+                object obj = ResourceManager.GetObject("popcorn3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap product_management {
             get {
                 object obj = ResourceManager.GetObject("product-management", resourceCulture);
