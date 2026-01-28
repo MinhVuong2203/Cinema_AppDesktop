2cbeb4aa385157e64080216124cb3f8c47efdbaa
sHunOrinn <161719997+sHunOrinn@users.noreply.github.com>
2026-01-28 11:50:06 +0700

    sửa chút

--- Arena_Cinema/UI/EmployeeSale/SelectMovieUC.Designer.cs
+++ Arena_Cinema/UI/EmployeeSale/SelectMovieUC.Designer.cs
@@ -15,22 +15,23 @@
 
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panelMain = new System.Windows.Forms.Panel();
             this.flpMovies = new System.Windows.Forms.FlowLayoutPanel();
-            this.panelHeader = new System.Windows.Forms.Panel();
-            this.lblDateTime = new System.Windows.Forms.Label();
-            this.lblMovieCount = new System.Windows.Forms.Label();
-            this.lbMovieListTitle = new System.Windows.Forms.Label();
-            this.btn_back = new ReaLTaiizor.Controls.ParrotButton();
             this.panelFilter = new System.Windows.Forms.Panel();
             this.cboGenre = new System.Windows.Forms.ComboBox();
             this.lblGenre = new System.Windows.Forms.Label();
             this.txtSearch = new System.Windows.Forms.TextBox();
             this.lblSearch = new System.Windows.Forms.Label();
-            this.timerClock = new System.Windows.Forms.Timer();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.lblDateTime = new System.Windows.Forms.Label();
+            this.lblMovieCount = new System.Windows.Forms.Label();
+            this.lbMovieListTitle = new System.Windows.Forms.Label();
+            this.btn_back = new ReaLTaiizor.Controls.ParrotButton();
+            this.timerClock = new System.Windows.Forms.Timer(this.components);
             this.panelMain.SuspendLayout();
-            this.panelHeader.SuspendLayout();
             this.panelFilter.SuspendLayout();
+            this.panelHeader.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMain
@@ -56,6 +57,72 @@
             this.flpMovies.Padding = new System.Windows.Forms.Padding(10);
             this.flpMovies.Size = new System.Drawing.Size(1590, 580);
             this.flpMovies.TabIndex = 2;
+            // 
+            // panelFilter
+            // 
+            this.panelFilter.BackColor = System.Drawing.Color.White;
+            this.panelFilter.Controls.Add(this.cboGenre);
+            this.panelFilter.Controls.Add(this.lblGenre);
+            this.panelFilter.Controls.Add(this.txtSearch);
+            this.panelFilter.Controls.Add(this.lblSearch);
+            this.panelFilter.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelFilter.Location = new System.Drawing.Point(20, 120);
+            this.panelFilter.Name = "panelFilter";
+            this.panelFilter.Padding = new System.Windows.Forms.Padding(20, 15, 20, 15);
+            this.panelFilter.Size = new System.Drawing.Size(1590, 80);
+            this.panelFilter.TabIndex = 1;
+            // 
+            // cboGenre
+            // 
+            this.cboGenre.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboGenre.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.cboGenre.FormattingEnabled = true;
+            this.cboGenre.Items.AddRange(new object[] {
+            "Tất cả",
+            "Hành động",
+            "Hài",
+            "Kinh dị",
+            "Tình cảm",
+            "Hoạt hình"});
+            this.cboGenre.Location = new System.Drawing.Point(611, 20);
+            this.cboGenre.Name = "cboGenre";
+            this.cboGenre.Size = new System.Drawing.Size(200, 33);
+            this.cboGenre.TabIndex = 3;
+            // 
+            // lblGenre
+            // 
+            this.lblGenre.AutoSize = true;
+            this.lblGenre.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblGenre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(41)))), ((int)(((byte)(55)))));
+            this.lblGenre.Location = new System.Drawing.Point(508, 23);
+            this.lblGenre.Name = "lblGenre";
+            this.lblGenre.Size = new System.Drawing.Size(86, 25);
+            this.lblGenre.TabIndex = 2;
+            this.lblGenre.Text = "Thể loại:";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.txtSearch.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(114)))), ((int)(((byte)(128)))));
+            this.txtSearch.Location = new System.Drawing.Point(150, 20);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(300, 32);
+            this.txtSearch.TabIndex = 1;
+            this.txtSearch.Text = "Nhập tên phim...";
+            this.txtSearch.TextChanged += new System.EventHandler(this.TxtSearch_TextChanged);
+            this.txtSearch.Enter += new System.EventHandler(this.TxtSearch_Enter);
+            this.txtSearch.Leave += new System.EventHandler(this.TxtSearch_Leave);
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblSearch.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(41)))), ((int)(((byte)(55)))));
+            this.lblSearch.Location = new System.Drawing.Point(20, 23);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(98, 25);
+            this.lblSearch.TabIndex = 0;
+            this.lblSearch.Text = "Tìm kiếm:";
             // 
             // panelHeader
             // 
@@ -89,7 +156,7 @@
             this.lblMovieCount.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(114)))), ((int)(((byte)(128)))));
             this.lblMovieCount.Location = new System.Drawing.Point(120, 60);
             this.lblMovieCount.Name = "lblMovieCount";
-            this.lblMovieCount.Size = new System.Drawing.Size(180, 25);
+            this.lblMovieCount.Size = new System.Drawing.Size(171, 25);
             this.lblMovieCount.TabIndex = 2;
             this.lblMovieCount.Text = "Đang chiếu 0 phim";
             // 
@@ -100,7 +167,7 @@
             this.lbMovieListTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(41)))), ((int)(((byte)(55)))));
             this.lbMovieListTitle.Location = new System.Drawing.Point(115, 15);
             this.lbMovieListTitle.Name = "lbMovieListTitle";
-            this.lbMovieListTitle.Size = new System.Drawing.Size(333, 46);
+            this.lbMovieListTitle.Size = new System.Drawing.Size(277, 46);
             this.lbMovieListTitle.TabIndex = 1;
             this.lbMovieListTitle.Text = "🎬 CHỌN PHIM";
             // 
@@ -128,72 +195,6 @@
             this.btn_back.Vertical_Alignment = System.Drawing.StringAlignment.Center;
             this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
             // 
-            // panelFilter
-            // 
-            this.panelFilter.BackColor = System.Drawing.Color.White;
-            this.panelFilter.Controls.Add(this.cboGenre);
-            this.panelFilter.Controls.Add(this.lblGenre);
-            this.panelFilter.Controls.Add(this.txtSearch);
-            this.panelFilter.Controls.Add(this.lblSearch);
-            this.panelFilter.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelFilter.Location = new System.Drawing.Point(20, 120);
-            this.panelFilter.Name = "panelFilter";
-            this.panelFilter.Padding = new System.Windows.Forms.Padding(20, 15, 20, 15);
-            this.panelFilter.Size = new System.Drawing.Size(1590, 80);
-            this.panelFilter.TabIndex = 1;
-            // 
-            // cboGenre
-            // 
-            this.cboGenre.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboGenre.Font = new System.Drawing.Font("Segoe UI", 11F);
-            this.cboGenre.FormattingEnabled = true;
-            this.cboGenre.Items.AddRange(new object[] {
-            "Tất cả",
-            "Hành động",
-            "Hài",
-            "Kinh dị",
-            "Tình cảm",
-            "Hoạt hình"});
-            this.cboGenre.Location = new System.Drawing.Point(570, 20);
-            this.cboGenre.Name = "cboGenre";
-            this.cboGenre.Size = new System.Drawing.Size(200, 33);
-            this.cboGenre.TabIndex = 3;
-            // 
-            // lblGenre
-            // 
-            this.lblGenre.AutoSize = true;
-            this.lblGenre.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
-            this.lblGenre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(41)))), ((int)(((byte)(55)))));
-            this.lblGenre.Location = new System.Drawing.Point(460, 23);
-            this.lblGenre.Name = "lblGenre";
-            this.lblGenre.Size = new System.Drawing.Size(95, 25);
-            this.lblGenre.TabIndex = 2;
-            this.lblGenre.Text = "Thể loại:";
-            // 
-            // txtSearch
-            // 
-            this.txtSearch.Font = new System.Drawing.Font("Segoe UI", 11F);
-            this.txtSearch.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(114)))), ((int)(((byte)(128)))));
-            this.txtSearch.Location = new System.Drawing.Point(140, 20);
-            this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(300, 32);
-            this.txtSearch.TabIndex = 1;
-            this.txtSearch.Text = "Nhập tên phim...";
-            this.txtSearch.Enter += new System.EventHandler(this.TxtSearch_Enter);
-            this.txtSearch.Leave += new System.EventHandler(this.TxtSearch_Leave);
-            this.txtSearch.TextChanged += new System.EventHandler(this.TxtSearch_TextChanged);
-            // 
-            // lblSearch
-            // 
-            this.lblSearch.AutoSize = true;
-            this.lblSearch.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
-            this.lblSearch.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(41)))), ((int)(((byte)(55)))));
-            this.lblSearch.Location = new System.Drawing.Point(20, 23);
-            this.lblSearch.Name = "lblSearch";
-            this.lblSearch.Size = new System.Drawing.Size(105, 25);
-            this.lblSearch.TabIndex = 0;
-            this.lblSearch.Text = "🔍 Tìm kiếm:";
-            // 
             // timerClock
             // 
             this.timerClock.Enabled = true;
@@ -209,11 +210,12 @@
             this.Name = "SelectMovieUC";
             this.Size = new System.Drawing.Size(1630, 800);
             this.panelMain.ResumeLayout(false);
-            this.panelHeader.ResumeLayout(false);
-            this.panelHeader.PerformLayout();
             this.panelFilter.ResumeLayout(false);
             this.panelFilter.PerformLayout();
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
             this.ResumeLayout(false);
+
         }
 
         #region Component Designer generated code
